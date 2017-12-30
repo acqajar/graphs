8790f4a4796a3667f22020004afd650e1c0f69d1
Arsames <acqajar@gmail.com>
2017-12-30 12:45:26 -0800

    glcoud storage

--- graphs/routes/index.js
+++ graphs/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
-var router = express.Router();
+var app = express.Router();
+const Storage = require("@google-cloud/storage");
+
+
+// Your Google Cloud Platform project ID
+const projectId = 'd3-graphs-1514664972177';
+
+// Creates a client
+const storage = new Storage({
+  projectId: projectId,
+});
+
+// The name for the new bucket
+const bucketName = 'd3graphtest';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
+app.post('/upload', function (req, res, next) {
+  // Uploads a local file to the bucket
+
+  var filename = req.body.filename
+
+  storage
+    .bucket(bucketName)
+    .upload(filename)
+    .then(() => {
+      console.log(`${filename} uploaded to ${bucketName}.`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+})
+
+module.exports = app;
